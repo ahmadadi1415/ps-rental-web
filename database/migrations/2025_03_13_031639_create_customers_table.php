f2e4878c09f129a2e1e67a757789bcbd34031b7b
ahmadadi1415 <adipras606@gmail.com>
2025-03-13 13:16:46 +0700

    [feat] add database seeder for product and user

--- database/migrations/2025_03_13_031639_create_customers_table.php
+++ database/migrations/2025_03_13_031639_create_customers_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('phone');
+            $table->string('phone')->nullable();
             $table->timestamps();
         });
     }
