a463fd2b497d8574c879b39adc3f077a5a798f29
ahmadadi1415 <adipras606@gmail.com>
2025-03-13 10:37:03 +0700

    [feat] add migration for table

---
+++ database/migrations/2025_03_13_031641_create_products_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->enum('console_type', ['PS4', 'PS5']); // PS4 or PS5
+            $table->integer('base_price');  // 30000 for PS4, 40000 for PS5
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
